de4b44948c3c05564d5339aec06c01d28f373a2b
Dima Kroma <37256188+dimakm@users.noreply.github.com>
2018-08-27 22:59:24 -0400

    Updating the map API code

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZGltYWtyb21hIiwiYSI6ImNqbDV3ODd1ZTA0cm8zdnBmdWttN2t1MWMifQ.QUSyJFPRRKXTt0uASnURCQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
